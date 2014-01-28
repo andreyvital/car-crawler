f88dcd76cbd603ccaec1534f44c09a073eb62864
Andrey Knupp Vital <andreykvital@gmail.com>
2014-01-28 17:05:43 -0200

    refatorando WebMotors; agregador de informações sobre marcas e carros

--- src/Crawler/WebMotors.php
+++ src/Crawler/WebMotors.php
@@ -1,49 +1,79 @@
 <?php
 
-namespace Crawler\dom;
+namespace Crawler;
 
+use Crawler\CarIndustryAggregator;
 use Crawler\http\CURL;
-
+use Crawler\data\Brand;
+use Crawler\data\Car;
 use DOMDocument;
 use DOMXPath;
 
-class WebMotors
+class WebMotors implements CarIndustryAggregator
 {
 
-    private $document;
-    private $xpath;
-    private $url = 'http://www.webmotors.com.br';
+  /**
+   * Endpoint que será usado para extraír a informação
+   * @const string
+   */
+  const ENDPOINT = 'http://www.webmotors.com.br';
 
-    public function __construct()
-    {
-        libxml_use_internal_errors(true);
-        
-        $this->document = new DOMDocument();
-        $this->document->loadHTMLFile($this->url);
-        $this->xpath = new DOMXPath($this->document);
+  public function __construct()
+  {
+    libxml_use_internal_errors(true);
+  }
+
+  /**
+   * Recupera todas as marcas
+   * 
+   * @return array
+   */
+  public function getBrands()
+  {
+    // ---------------------------------------
+    $dom = new DOMDocument('1.0', 'UTF-8');
+    $dom->preserveWhiteSpace = true;
+
+    $dom->loadHTMLFile(WebMotors::ENDPOINT);
+    $xpath = new DOMXPath($dom);
+    // ---------------------------------------
+
+    $brands = array();
+
+    foreach ($xpath->query('//*[@id="IdMarca"]/option[number(@value) > 0]') as $node) {
+      $brand = new Brand(
+        $node->getAttribute('value'), 
+        $node->nodeValue
+      );
+
+
+      foreach ($this->getCars($brand) AS $vehicle) {
+        $car = new Car($vehicle->Nome, $brand);
+        $car->setId($vehicle->Id);
+
+        $brand->addCar($car);
+      }
     }
 
-    public function getBrands()
-    {
-        return $this->xpath->query('//*[@id="IdMarca"]/option[number(@value) = number(@value) and number(@value) > 0]');
-    }
+    return $brands;
+  }
 
-    public function getBrand($id)
-    {
-        return $this->xpath->query("//*[@id='IdMarca']/option[@value={$id}]")->item(0); 
-    }
+  /**
+   * Recupera todos os carros de uma determinada marca
+   * 
+   * @param Brand $brand marca
+   * @return array
+   */
+  private function getCars(Brand $brand)
+  {
+    $curl = new CURL('http://www.webmotors.com.br/carro/modelos');
+    $curl->setMethod('POST');
+    $curl->setFields(array('marca' => $brand->getId()));
 
-    public function getCars($brand)
-    {
-        $brand = $this->getBrand($brand)->getAttribute('value');
-        $curl = new CURL('http://www.webmotors.com.br/carro/modelos');
-        $curl->setMethod('POST');
-        $curl->setFields(array('marca' => $brand));
+    $response = json_decode($curl->getResponse());
+    $curl->close();
 
-        $response = $curl->getResponse();
-        $curl->close();
-
-        return $response;
-    }
+    return $response;
+  }
 
 }
